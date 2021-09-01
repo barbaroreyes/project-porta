f3c449893ec3351f7f2229c329d2c16fd2d5259f
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-01 17:55:37 -0500

    style-to-name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='name'>Barbaro Reyes</h1>
+        <h1 className='name grow '>Barbaro Reyes</h1>
       </header>
     </div>
   );
