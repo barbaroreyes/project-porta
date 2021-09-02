ca8d1971d1d2af91ad6f09a87bee319ff1df73da
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-02 08:17:02 -0500

    added-nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Nav from './component/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <Nav/>
         <h1 className='name grow '>Barbaro Reyes</h1>
       </header>
     </div>
---
+++ src/component/Nav.js
@@ -0,0 +1,9 @@
+import {Link} from 'react-router-dom'
+const Home = () => {
+    return(
+        <nav className='nav'>
+            <Link to="/">Home</Link>
+        </nav>
+    )
+}
+export default  Home
